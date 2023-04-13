857acbe4cd44e0641f14d2b3e974f96a9fc3c0aa
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-13 21:21:46 +0200

    button to the left side for more space

--- assets/js/initiater.js
+++ assets/js/initiater.js
@@ -10,8 +10,8 @@ function init() {
 function setUpCanvas() {
     _$canvas = document.querySelector('canvas');
     _ctx = _$canvas.getContext('2d');
-    _$canvas.width = 800;
-    _$canvas.height = 800;
+    _$canvas.width = 850;
+    _$canvas.height = 850;
     drawRect('#FFD3B6', [0, 0, _$canvas.width, _$canvas.height]);
 }
 
