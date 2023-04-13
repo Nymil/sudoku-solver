5bde740245ef81b443b5eb74772701b0832f9979
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-13 15:12:16 +0200

    canvas setup

--- assets/js/initiater.js
+++ assets/js/initiater.js
@@ -3,5 +3,13 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('js connected');
+    setUpCanvas();
+}
+
+function setUpCanvas() {
+    _$canvas = document.querySelector('canvas');
+    _ctx = _$canvas.getContext('2d');
+    _$canvas.width = 800;
+    _$canvas.height = 800;
+    drawRect('#FFD3B6', [0, 0, _$canvas.width, _$canvas.height]);
 }
