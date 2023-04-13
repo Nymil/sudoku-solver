a432394fd49621023021edc681303d8cded767d7
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-13 20:59:04 +0200

    run method main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
 class Main {
     constructor() {
+        this.fps = 60;
+    }
+
+    update() {
+
+    }
+
+    draw() {
         
+    }
+
+    run() {
+        // main game loop
+        setInterval(() => {
+            this.update();
+            this.draw();
+        }, 1000 / this.fps);
     }
 }
